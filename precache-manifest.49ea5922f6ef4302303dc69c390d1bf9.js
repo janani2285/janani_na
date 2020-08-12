901d856dab025921a5bbd65a7d943f0d203f084a
Janani NA <janani2285@gmail.com>
2020-08-12 15:58:51 -0700

    Updates

---
+++ precache-manifest.49ea5922f6ef4302303dc69c390d1bf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7250190148b1b9befdedf26a99ca0616",
+    "url": "/janani_na/index.html"
+  },
+  {
+    "revision": "beb1a9025350c66bbb11",
+    "url": "/janani_na/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "e91d211c84a977c10e1a",
+    "url": "/janani_na/static/js/2.07202ae0.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/janani_na/static/js/2.07202ae0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beb1a9025350c66bbb11",
+    "url": "/janani_na/static/js/main.a64139b3.chunk.js"
+  },
+  {
+    "revision": "34c5de139e3623871331",
+    "url": "/janani_na/static/js/runtime-main.ef3b4b8f.js"
+  }
+]);
