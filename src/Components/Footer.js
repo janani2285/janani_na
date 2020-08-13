f52666406c287c33189d3f805d20d9559efdcc33
Janani NA <janani2285@gmail.com>
2020-08-12 17:08:28 -0700

    removed all default tempate name and updated my name, linkedin url updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -19,7 +19,7 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; Copyright 2017 Tim Baker</li>
+              <li>&copy; Copyright 2017 Janani Nagaraj Amutha</li>
               <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
            </ul>
 
